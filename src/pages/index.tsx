6effc5e98001de6e9b74a59d8e95301fd523e509
JEAN GAL <j.c.r.gal@student.tudelft.nl>
2022-12-02 18:35:27 +0100

    Read smart contract value / show sale Status

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,52 @@
-import { Flex, Spacer, Text, Link, Box, HStack, useNumberInput, Button } from '@chakra-ui/react'
+import { Flex, Spacer, Text, Link, Box, HStack, useNumberInput } from '@chakra-ui/react'
 import { WalletBox } from '../components/boxes/WalletBox'
 import { QuantityBox } from '../components/boxes/QuantityBox'
 import { PriceBox } from '../components/boxes/PriceBox'
 import { BackButton } from '../components/buttons/BackButton'
 import { LicenseImage } from '../components/LicenseImage'
 import { InfoBanner } from '../components/InfoBanner'
+import { useEffect, useState } from 'react'
+import config from '../contractConfig.json'
+
+import { useContractRead } from 'wagmi'
 
 function Page() {
 
-  const sale = "Public";
-
+  const [status, setStatus] = useState("");
   const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
     useNumberInput({
       step: 1,
-      defaultValue: 1,
+      defaultValue: 2,
       min: 0,
       max: 2
     })
-    
+
+  const { data, isError, isLoading } = useContractRead({
+    address: config.address,
+    abi: config.abi,
+    functionName: "saleStatus"
+  })
+
+  function switchStatus(value: Number) {
+    switch(value) {
+      case 0:
+        return "Locked";
+      case 1:
+        return "WHITELIST SALE"
+      case 2: 
+        return "PUBLIC SALE"
+      default:
+        return ""
+    }
+  }
+
+  useEffect(() => {
+    if(data instanceof Number) {
+      const currentStatus = switchStatus(data);
+      setStatus(currentStatus);
+    }
+  }, [])
+
   const inc = getIncrementButtonProps()
   const dec = getDecrementButtonProps()
   const amount = getInputProps()
@@ -40,8 +69,8 @@ function Page() {
         <HStack ml="1%" mt="1%" textColor='white'>
           <BackButton />
           <Link href='https://nftouring.io' fontWeight="bold" fontSize="lg" color='white'>WEBSITE</Link>
-          <Box w='2%' />
-          <Text>PRESALE / PUBLIC SALE</Text>
+          <Box w="5px" />
+          <Text fontWeight="bold" color="#94d5f2" fontSize="sm">{status}</Text>
         </HStack>
         <Spacer />
         <Flex h="7%" justifyContent="center">
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,12 @@
 import { getDefaultClient } from 'connectkit'
 import { createClient, chain } from 'wagmi'
 
-const chains = [chain.mainnet];
+const chains = [chain.mainnet, chain.goerli];
 
 export const client = createClient(
   getDefaultClient({
     appName: "NFTouring Minting Page",
     autoConnect: true,
     chains
-  }),
+  })
 )
